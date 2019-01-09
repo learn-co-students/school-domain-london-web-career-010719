bec0418da4e97fd0cd994f394819b55afa75ce4a
Minha Koo <kkooming2@gmail.com>
2019-01-09 15:14:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+# a ||= b is a "conditional assignment operator"
+# "if a is undefined or falsey (false or nil), then evaluate b and set a to the result".
+
+  def add_student(name, grade)
+    # roster[grade] ||= []
+    # roster[grade] << name
+    
+    roster[grade] = [] if roster[grade] == nil
+    roster[grade] << name
+  end
+
+  def grade(grade_num)
+    roster[grade_num]
+  end
+
+  def sort 
+    roster.each do |grade, name_arr|
+      name_arr.sort!
+    end
+  end
+end
